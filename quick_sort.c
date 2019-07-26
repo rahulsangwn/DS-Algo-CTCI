74695836874d2c378ce8563151fac29e4959237f
rahulsangwn <rahul.sangwan3@gmail.com>
2019-07-26 14:57:31 +0530

    after quick sort

---
+++ quick_sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void quicksort (int *, int, int);
+int main () {
+    int i, arr[] = {45, 62, 91, 18, 88, 72, 11, 10, 29, 54};
+    
+    quicksort (arr, 0, 9);
+
+    for (i = 0; i < 10; i++) {
+        printf ("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+void quicksort (int *arr, int start, int end) {
+    int num = start + 1, ptr = start + 1, temp;
+    for (; num <= end; num++) {
+        if (arr[start] >= arr[num]) {
+            temp = arr[num];
+            arr[num] = arr[ptr];
+            arr[ptr] = temp;
+            ptr++; 
+        } 
+    }
+    temp = arr[ptr-1];
+    arr[ptr-1] = arr[start];
+    arr[start] = temp;
+
+    if (ptr-1 != end) {
+        quicksort (arr, ptr, end);
+    }
+    if (start != ptr-1) {
+        quicksort (arr, start, ptr-2);
+    }
+}
