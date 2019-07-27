537dc2516f678c5fe666f67983ba435056d3d43b
rahulsangwn <rahul.sangwan3@gmail.com>
2019-07-27 20:46:58 +0530

    printing execution time

--- iterative_matrix_multiplication.c
+++ iterative_matrix_multiplication.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <time.h>
 
 int main () {
+    clock_t begin = clock ();
+
     int arr1[][3] = {{1, 2, 3}, {4, 5, 6}};
     int arr2[][4] = {{7, 8, 9, 10}, {11, 12, 13, 14}, {15, 16, 17, 18}};
     int i, j, k, res[2][4];
@@ -25,4 +28,9 @@ int main () {
         }
         printf ("\n");
     }
+
+    clock_t end = clock ();
+    double time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
+
+    printf("\n Time take by the Program (in sec): %lf\n", time_spent);
 }
