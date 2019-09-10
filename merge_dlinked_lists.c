4a3fbb0d322918a565ba3d6e7a020168645f2031
rahulsangwn <rahul.sangwan3@gmail.com>
2019-09-10 22:12:03 +0530

    merging of two sorted doubly linked lists

--- merge_dlinked_lists.c
+++ merge_dlinked_lists.c
@@ -1,14 +1,54 @@
 #include "dlinkedlist.h"
 
+void dmerge (doubly **head, doubly **head2) {
+    doubly *ptr, *h1 = *head, *h2 = *head2, *tail = (doubly *) malloc (sizeof(doubly));
+    ptr = tail;
+    tail->prev = NULL; 
+    tail->data = '\0';
+    while (h1 != NULL && h2 != NULL) {
+        if (h1->data <= h2->data) {
+            tail->next = h1;
+            h1->prev = tail;
+            h1 = h1->next;
+            tail = tail->next;
+            tail->next = NULL;
+        } else {
+            tail->next = h2;
+            h2->prev = tail;
+            h2 = h2->next;
+            tail = tail->next;
+            tail->next = NULL;
+        }
+    }
+
+    if (h1 == NULL) {
+        tail->next = h2;
+        h2->prev = tail;
+    } else {
+        tail->next = h1;
+        h1->prev = tail;
+    }
+
+    *head = ptr->next;
+}
+
 int main() {
-    doubly *head = NULL;
+    doubly *head = NULL, *head2 = NULL;
 
     dinsert_front(&head, 2);
     dinsert_front(&head, 4);
     dinsert_front(&head, 11);
     dinsert_front(&head, 15);
+    dreverse(&head);    
 
-    dreverse(&head);
+    dinsert_front(&head2, 5);
+    dinsert_front(&head2, 11);
+    dinsert_front(&head2, 14);
+    dinsert_front(&head2, 18);
+    dreverse(&head2);
+
+    dmerge(&head, &head2);
 
     ddisplay(head);
+    printf ("\n");
 }
