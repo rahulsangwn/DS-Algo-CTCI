4354b2760e6bab28f75fc5faf9dcbecaf60fe3dc
rahulsangwn <rahul.sangwan3@gmail.com>
2019-09-18 16:42:08 +0530

    finding a missing element from the array

--- java/src/ArrayMissingElement.java
+++ java/src/ArrayMissingElement.java
@@ -1,5 +1,27 @@
 public class ArrayMissingElement {
-    public static void main(String[] args) {
 
+    private static int offset;
+    public static void main(String[] args) {
+        int arr[] = {3, 4, 5, 6, 8, 9, 10};
+        offset = arr[0];
+        System.out.println("The missing element is: " + findArrayElement(arr, 0, arr.length - 1));;
+    }
+
+    private static int findArrayElement (int[] arr, int start, int end) {
+        if (arr[start] + 2 == arr[start+1])
+            return arr[start] + 1;
+        else {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == mid + offset) {
+                int result = findArrayElement(arr, mid, end);
+                return result;
+            } else {
+                int result = findArrayElement(arr, start, mid -1 );
+                return result;
+            }
+        }
     }
 }
+
+
