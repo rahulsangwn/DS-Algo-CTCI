8b6193091c378b6fbef0ffd8d7f7e415e9a125a9
rahulsangwn <rahul.sangwan3@gmail.com>
2019-10-10 20:43:20 +0530

    finding kth smallest element

---
+++ java/src/KthSmallestElement.java
@@ -0,0 +1,15 @@
+// Source: https://www.geeksforgeeks.org/kth-smallestlargest-element-unsorted-array/
+// Time Complexity: O(n)
+
+public class KthSmallestElement {
+    public static void main(String args[]) {
+        int arr[] = {7, 10, 4, 3, 20, 15};
+        int k = 3;
+
+        System.out.println(findKth(arr, k));
+    }
+
+    private static int findKth(int arr[], int k) {
+
+    }
+}
