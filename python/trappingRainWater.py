11a57b72ee458eaf272442e0bbc7797620e9327f
rahulsangwn <rahul.sangwan3@gmail.com>
2019-10-29 22:50:38 +0530

    trapping rain water order of n^2

---
+++ python/trappingRainWater.py
@@ -0,0 +1,36 @@
+list = [1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+
+
+def main():
+    result = 0
+    for i in range(0, len(list)):
+        number = min(lmax(i), rmax(i))
+        if(number >= list[i]):
+            result += number - list[i]
+    print(result)
+
+
+def lmax(i):
+    l = list[: i]
+    if len(l) == 0:
+        return 0
+    else:
+        return max(l)
+
+
+
+
+
+def rmax(i):
+    l = list[i+1: len(list)]
+
+    if len(l) == 0:
+        return 0
+    else:
+        return max(l)
+
+
+main()
+
+
+
